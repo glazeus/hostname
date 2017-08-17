335e644e32f4d78a2216d1e61e0bebe53e17b6a2
RJ <glazeus@gmail.com>
2017-08-17 11:51:28 +0200

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -1,5 +1,5 @@
 #Display hostname and IP(s) for current container
 echo "Hostname: $HOSTNAME | IP(s): $(hostname -I)" > '/usr/local/apache2/htdocs/index.html'
 
-#Run web server
+#Run web server in background
 httpd -DFOREGROUND
